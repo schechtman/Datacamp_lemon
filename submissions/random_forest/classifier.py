332387b25e38b3b784f5919d0782dbd8bc811ba6
schechtman <sch.sholom@gmail.com>
2018-01-27 22:13:13 +0100

    presentation of the problem + model section in the notebook

--- submissions/random_forest/classifier.py
+++ submissions/random_forest/classifier.py
@@ -1,19 +1,19 @@
-from sklearn.linear_model import LogisticRegression
+
+
 from sklearn.preprocessing import Imputer
-from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator
 from sklearn.ensemble import RandomForestClassifier
 
 from imblearn.pipeline import make_pipeline as make_pipeline_imb
-from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
 
 class Classifier(BaseEstimator):
     def __init__(self):
         self.clf = make_pipeline_imb(
-        	Imputer(strategy='median'),
-            RandomUnderSampler(),
+            Imputer(strategy='median'),
+            RandomOverSampler(),
             RandomForestClassifier(10, verbose=True, min_impurity_decrease=10e-5)
-        	)
+            )
 
     def fit(self, X, y):
         self.clf.fit(X, y)
--- submissions/starting_kit/classifier.py
+++ submissions/starting_kit/classifier.py
@@ -1,18 +1,18 @@
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import Imputer
-from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator
 
 from imblearn.pipeline import make_pipeline as make_pipeline_imb
-from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
 
 
 class Classifier(BaseEstimator):
     def __init__(self):
         self.clf = make_pipeline_imb(
-        	Imputer(strategy='median'), 
-        	RandomUnderSampler(), 
-        	LogisticRegression(C=1e-3, penalty="l2"))
+            Imputer(strategy='median'), 
+            RandomOverSampler(), 
+            LogisticRegression(C=1e-3, penalty="l2"))
 
     def fit(self, X, y):
         self.clf.fit(X, y)
